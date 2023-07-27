f39218681b7b302f37af59b6bc0271b1fcb5a2f7
hermandev <hermantolakoro@gmail.com>
2023-07-28 04:36:20 +0800

    fix: update cron async

--- src/main/java/id/co/klikdata/satusehat/config/CronConfig.java
+++ src/main/java/id/co/klikdata/satusehat/config/CronConfig.java
@@ -1,9 +1,11 @@
 package id.co.klikdata.satusehat.config;
 
 import id.co.klikdata.satusehat.dao.GrupRuanganDao;
+import id.co.klikdata.satusehat.dao.PendaftaranDao;
 import id.co.klikdata.satusehat.dao.RuanganDao;
 import id.co.klikdata.satusehat.dao.UsersDao;
 import id.co.klikdata.satusehat.entity.GrupRuangan;
+import id.co.klikdata.satusehat.entity.Pendaftaran;
 import id.co.klikdata.satusehat.entity.Ruangan;
 import id.co.klikdata.satusehat.entity.Users;
 import id.co.klikdata.satusehat.service.LocationService;
@@ -12,6 +14,9 @@ import id.co.klikdata.satusehat.service.PasienService;
 import id.co.klikdata.satusehat.service.PractitionerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -19,17 +24,20 @@ import java.util.List;
 
 @Configuration
 @EnableScheduling
+@EnableAsync
 @RequiredArgsConstructor
 public class CronConfig {
     private final UsersDao usersDao;
     private final GrupRuanganDao grupRuanganDao;
     private final RuanganDao ruanganDao;
+    private final PendaftaranDao pendaftaranDao;
     private final PasienService pasienService;
     private final PractitionerService practitionerService;
     private final OrganizationService organizationService;
     private final LocationService locationService;
 
 
+    @Async
     @Scheduled(fixedRate = 50000)
     public void pasienCron() {
         System.out.println("Cron Pasien Running!");
@@ -40,6 +48,7 @@ public class CronConfig {
         });
     }
 
+    @Async
     @Scheduled(fixedRate = 10000)
     public void organizationCron() {
         System.out.println("Cron Organization Running!");
@@ -51,6 +60,7 @@ public class CronConfig {
     }
 
 
+    @Async
     @Scheduled(fixedRate = 5000)
     public void practitionerCron() {
         System.out.println("Cron Practition Running!");
@@ -61,6 +71,7 @@ public class CronConfig {
         });
     }
 
+    @Async
     @Scheduled(fixedRate = 15000)
     public void locationCron() {
         System.out.println("Cron Location Running!");
@@ -68,6 +79,17 @@ public class CronConfig {
         data.forEach(item -> {
             System.out.println(item.getNamaRuangan());
             locationService.addLocation(item.getId());
+        });
+    }
+
+    @Async
+    @Scheduled(fixedRate = 16000)
+    public void encounterCron() {
+        System.out.println("Cron Encounter Running!");
+//        PageRequest pageRequest = PageRequest.of(3, 30);
+        List<Pendaftaran> data = pendaftaranDao.findAll();
+        data.forEach(item -> {
+            System.out.println(item.getNoRegister());
         });
     }
 
--- src/main/java/id/co/klikdata/satusehat/service/EncounterServiceImpl.java
+++ src/main/java/id/co/klikdata/satusehat/service/EncounterServiceImpl.java
@@ -35,65 +35,65 @@ public class EncounterServiceImpl implements EncounterService {
         Settings settings = settingsDao.findAll().get(0);
         Pendaftaran pendaftaran = pendaftaranDao.findById(new BigInteger(nomor)).get();
 
-        EncounterRequest req = new EncounterRequest();
+//        EncounterRequest req = new EncounterRequest();
+//
+//        req.setResourceType("Encounter");
+//        req.setStatus("arrived");
+//        req.setEncounterClass(new EncounterRequest.ClassDetails("http://terminology.hl7.org/CodeSystem/v3-ActCode", "AMB", "ambulatory"));
+//        req.setSubject(new EncounterRequest.Subject("Patient/" + pendaftaran.getPasien().getIdPasienIhs(), pendaftaran.getPasien().getNamaUser()));
+//
+//        List<EncounterRequest.Type> types = new ArrayList<>();
+//        List<EncounterRequest.Coding> codings = new ArrayList<>();
+//        codings.add(new EncounterRequest.Coding("http://terminology.hl7.org/CodeSystem/v3-ParticipationType", "ATND", "attender"));
+//        types.add(new EncounterRequest.Type(codings));
+//
+//        String namaDokter = pendaftaran.getDokter().getGelarDpn() + " " + pendaftaran.getDokter().getNamaUser() + ", " + pendaftaran.getDokter().getGelarBlk();
+//         EncounterRequest.Individual individual = new EncounterRequest.Individual("Practitioner/" + pendaftaran.getDokter().getIdPetugasIhs() , namaDokter);
+//
+//        List<EncounterRequest.Participant> participants = new ArrayList<>();
+//        participants.add(new EncounterRequest.Participant(types, individual));
+//        req.setParticipant(participants);
+//
+//        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
+//        OffsetDateTime offsetDateTime = pendaftaran.getTglBerobat().atOffset(ZoneOffset.ofHours(7));
+//        String tanggalBerobat = offsetDateTime.format(outputFormatter);
+//
+//
+//        req.setPeriod(new EncounterRequest.Period(tanggalBerobat));
+//
+//
+//        System.out.println("Location/" + pendaftaran.getRuangan().getIdIhs());
+//        List<EncounterRequest.LocationWrapper> locations = new ArrayList<>();
+//        locations.add(new EncounterRequest.LocationWrapper(new EncounterRequest.Location("Location/" + pendaftaran.getRuangan().getIdIhs(), pendaftaran.getRuangan().getNamaRuangan())));
+//        req.setLocation(locations);
+//
+//        List<EncounterRequest.StatusHistory> statusHistories = new ArrayList<>();
+//        statusHistories.add(new EncounterRequest.StatusHistory("arrived", new EncounterRequest.Period(tanggalBerobat)));
+//        req.setStatusHistory(statusHistories);
+//
+//        req.setServiceProvider(new EncounterRequest.ServiceProvider("Organization/" + settings.getOrganizationId()));
+//
+//        List<EncounterRequest.Identifier> identifiers = new ArrayList<>();
+//        identifiers.add(new EncounterRequest.Identifier("http://sys-ids.kemkes.go.id/encounter/" + settings.getOrganizationId(), pendaftaran.getNoTiket()));
+//        req.setIdentifier(identifiers);
+//
+//        String token = satuSehatService.getAccessToken().getAccessToken();
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.APPLICATION_JSON);
+//        headers.setBearerAuth(token);
+//        HttpEntity<EncounterRequest> request = new HttpEntity<>(req, headers);
+//        ResponseEntity<EncounterResponse> response = restTemplate.exchange(
+//                satuSehatService.getBaseUrl() + "/fhir-r4/v1/Encounter",
+//                HttpMethod.POST, request,
+//                EncounterResponse.class);
+//
+//        if (response.getStatusCode().equals(HttpStatus.CREATED)) {
+//            KodeIhs ihs = new KodeIhs();
+//            ihs.setNoRegister(pendaftaran.getNoRegister());
+//            ihs.setIhsEncounter(String.valueOf(response.getBody().getId()));
+//            kodeIhsDao.save(ihs);
+//        }
 
-        req.setResourceType("Encounter");
-        req.setStatus("arrived");
-        req.setEncounterClass(new EncounterRequest.ClassDetails("http://terminology.hl7.org/CodeSystem/v3-ActCode", "AMB", "ambulatory"));
-        req.setSubject(new EncounterRequest.Subject("Patient/" + pendaftaran.getPasien().getIdPasienIhs(), pendaftaran.getPasien().getNamaUser()));
-
-        List<EncounterRequest.Type> types = new ArrayList<>();
-        List<EncounterRequest.Coding> codings = new ArrayList<>();
-        codings.add(new EncounterRequest.Coding("http://terminology.hl7.org/CodeSystem/v3-ParticipationType", "ATND", "attender"));
-        types.add(new EncounterRequest.Type(codings));
-
-        String namaDokter = pendaftaran.getDokter().getGelarDpn() + " " + pendaftaran.getDokter().getNamaUser() + ", " + pendaftaran.getDokter().getGelarBlk();
-         EncounterRequest.Individual individual = new EncounterRequest.Individual("Practitioner/" + pendaftaran.getDokter().getIdPetugasIhs() , namaDokter);
-
-        List<EncounterRequest.Participant> participants = new ArrayList<>();
-        participants.add(new EncounterRequest.Participant(types, individual));
-        req.setParticipant(participants);
-
-        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
-        OffsetDateTime offsetDateTime = pendaftaran.getTglBerobat().atOffset(ZoneOffset.ofHours(7));
-        String tanggalBerobat = offsetDateTime.format(outputFormatter);
-
-
-        req.setPeriod(new EncounterRequest.Period(tanggalBerobat));
-
-
-        System.out.println("Location/" + pendaftaran.getRuangan().getIdIhs());
-        List<EncounterRequest.LocationWrapper> locations = new ArrayList<>();
-        locations.add(new EncounterRequest.LocationWrapper(new EncounterRequest.Location("Location/" + pendaftaran.getRuangan().getIdIhs(), pendaftaran.getRuangan().getNamaRuangan())));
-        req.setLocation(locations);
-
-        List<EncounterRequest.StatusHistory> statusHistories = new ArrayList<>();
-        statusHistories.add(new EncounterRequest.StatusHistory("arrived", new EncounterRequest.Period(tanggalBerobat)));
-        req.setStatusHistory(statusHistories);
-
-        req.setServiceProvider(new EncounterRequest.ServiceProvider("Organization/" + settings.getOrganizationId()));
-
-        List<EncounterRequest.Identifier> identifiers = new ArrayList<>();
-        identifiers.add(new EncounterRequest.Identifier("http://sys-ids.kemkes.go.id/encounter/" + settings.getOrganizationId(), pendaftaran.getNoTiket()));
-        req.setIdentifier(identifiers);
-
-        String token = satuSehatService.getAccessToken().getAccessToken();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setBearerAuth(token);
-        HttpEntity<EncounterRequest> request = new HttpEntity<>(req, headers);
-        ResponseEntity<EncounterResponse> response = restTemplate.exchange(
-                satuSehatService.getBaseUrl() + "/fhir-r4/v1/Encounter",
-                HttpMethod.POST, request,
-                EncounterResponse.class);
-
-        if (response.getStatusCode().equals(HttpStatus.CREATED)) {
-            KodeIhs ihs = new KodeIhs();
-            ihs.setNoRegister(pendaftaran.getNoRegister());
-            ihs.setIhsEncounter(String.valueOf(response.getBody().getId()));
-            kodeIhsDao.save(ihs);
-        }
-
-        return response.getBody();
+        return null;
     }
 }
