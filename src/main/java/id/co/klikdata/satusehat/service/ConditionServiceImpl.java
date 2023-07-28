6e9778229792a406483c3492ad16457aa7538101
hermandev <hermantolakoro@gmail.com>
2023-07-28 09:02:13 +0800

    fix: update cron async

--- src/main/java/id/co/klikdata/satusehat/service/ConditionServiceImpl.java
+++ src/main/java/id/co/klikdata/satusehat/service/ConditionServiceImpl.java
@@ -39,48 +39,48 @@ public class ConditionServiceImpl implements ConditionService {
         Pendaftaran pendaftaran = pendaftaranDao.findById(new BigInteger(nomor)).get();
         Diagnosa diagnosa = diagnosaDao.findByNoRegister(pendaftaran.getNoRegister());
         KodeIhs kode = kodeIhsDao.findById(pendaftaran.getNoRegister()).get();
-//
-//        ConditionRequest req = new ConditionRequest();
-//
-//        req.setResourceType("Condition");
-//
-//        List<ConditionRequest.Coding> codings = new ArrayList<>();
-//        codings.add(new ConditionRequest.Coding("http://terminology.hl7.org/CodeSystem/condition-clinical", "active", "Active"));
-//        req.setClinicalStatus(new ConditionRequest.ClinicalStatus(codings));
-//
-//        List<ConditionRequest.CategoryWrapper> categoryWrappers = new ArrayList<>();
-//        List<ConditionRequest.Coding> codings1 = new ArrayList<>();
-//        codings1.add(new ConditionRequest.Coding("http://terminology.hl7.org/CodeSystem/condition-category", "encounter-diagnosis", "Encounter Diagnosis"));
-//        categoryWrappers.add(new ConditionRequest.CategoryWrapper(codings1));
-//        req.setCategory(categoryWrappers);
-//
-//        List<ConditionRequest.Coding> codings2 = new ArrayList<>();
-//        codings2.add(new ConditionRequest.Coding("http://hl7.org/fhir/sid/icd-10", diagnosa.getDiagnosaDokter().getIdCode(), diagnosa.getDiagnosaDokter().getKetEnglish()));
-//        req.setCode(new ConditionRequest.CodeWrapper(codings2));
-//
-//        String hari = pendaftaran.getTglBerobat().getDayOfWeek().getDisplayName(TextStyle.FULL, new Locale("id", "ID"));
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy", new Locale("id", "ID"));
-//
-//        req.setSubject(new ConditionRequest.Subject("Patient/" + pendaftaran.getPasien().getIdPasienIhs(), pendaftaran.getPasien().getNamaUser()));
-//        req.setEncounter(new ConditionRequest.EncounterWrapper("Encounter/" + pendaftaran.getIdIhs(), "Kunjungan " + pendaftaran.getPasien().getNamaUser() + " di hari " + hari +", " + pendaftaran.getTglBerobat().format(formatter)));
-//
-//        String token = satuSehatService.getAccessToken().getAccessToken();
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON);
-//        headers.setBearerAuth(token);
-//        HttpEntity<ConditionRequest> request = new HttpEntity<>(req, headers);
-//        ResponseEntity<ConditionResponse> response = restTemplate.exchange(
-//                satuSehatService.getBaseUrl() + "/fhir-r4/v1/Condition",
-//                HttpMethod.POST, request,
-//                ConditionResponse.class);
-//
-//
-//        if (response.getStatusCode().equals(HttpStatus.CREATED)) {
-//            kode.setIhsCondition(Objects.requireNonNull(response.getBody()).getId());
-//            kodeIhsDao.save(kode);
-//        }
+
+        ConditionRequest req = new ConditionRequest();
+
+        req.setResourceType("Condition");
+
+        List<ConditionRequest.Coding> codings = new ArrayList<>();
+        codings.add(new ConditionRequest.Coding("http://terminology.hl7.org/CodeSystem/condition-clinical", "active", "Active"));
+        req.setClinicalStatus(new ConditionRequest.ClinicalStatus(codings));
+
+        List<ConditionRequest.CategoryWrapper> categoryWrappers = new ArrayList<>();
+        List<ConditionRequest.Coding> codings1 = new ArrayList<>();
+        codings1.add(new ConditionRequest.Coding("http://terminology.hl7.org/CodeSystem/condition-category", "encounter-diagnosis", "Encounter Diagnosis"));
+        categoryWrappers.add(new ConditionRequest.CategoryWrapper(codings1));
+        req.setCategory(categoryWrappers);
+
+        List<ConditionRequest.Coding> codings2 = new ArrayList<>();
+        codings2.add(new ConditionRequest.Coding("http://hl7.org/fhir/sid/icd-10", diagnosa.getDiagnosaDokter().getIdCode(), diagnosa.getDiagnosaDokter().getKetEnglish()));
+        req.setCode(new ConditionRequest.CodeWrapper(codings2));
+
+        String hari = pendaftaran.getTglBerobat().getDayOfWeek().getDisplayName(TextStyle.FULL, new Locale("id", "ID"));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy", new Locale("id", "ID"));
+
+        req.setSubject(new ConditionRequest.Subject("Patient/" + pendaftaran.getPasien().getIdPasienIhs(), pendaftaran.getPasien().getNamaUser()));
+        req.setEncounter(new ConditionRequest.EncounterWrapper("Encounter/" + pendaftaran.getIdIhs(), "Kunjungan " + pendaftaran.getPasien().getNamaUser() + " di hari " + hari +", " + pendaftaran.getTglBerobat().format(formatter)));
+
+        String token = satuSehatService.getAccessToken().getAccessToken();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(token);
+        HttpEntity<ConditionRequest> request = new HttpEntity<>(req, headers);
+        ResponseEntity<ConditionResponse> response = restTemplate.exchange(
+                satuSehatService.getBaseUrl() + "/fhir-r4/v1/Condition",
+                HttpMethod.POST, request,
+                ConditionResponse.class);
 
 
-        return null;
+        if (response.getStatusCode().equals(HttpStatus.CREATED)) {
+            kode.setIhsCondition(Objects.requireNonNull(response.getBody()).getId());
+            kodeIhsDao.save(kode);
+        }
+
+
+        return response.getBody();
     }
 }
